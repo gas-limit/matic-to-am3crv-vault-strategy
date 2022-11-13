31185ccb802dc1a2e5c69cb5f2c0af1fb6ffe5d6
Josh <100609687+JoshWeb3@users.noreply.github.com>
2022-11-13 15:02:43 -0500

    Update firstVault.sol

--- firstVault.sol
+++ firstVault.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
     //Strategy:
     // 1. Users deposit ETH 
     // 2. ETH is swapped for DAI, USDC, and USDT on on Uniswap V2
@@ -9,20 +11,27 @@ pragma solidity ^0.8.13;
     // 4. aDAI, aUSDC, and aUSDT is deposited into Curve AAVE Stablecoin pool
     //      Curve gives am3CRV tokens
 
-
-    // **How to Execute
+    //Instructions
+    //**How to Execute
     //1. A_ETHToStablesUniswap() - swap from eth to USDC, USDT, and DAI from a DEX
     //2. B_DepositIntoAAVE() - deposit USDC, USDT, and DAI into AAVE
     //3. C_DepositIntoCurve() - deposit aUSDC, aUSDT, and aDAI into Curve's AAVE stablecoin pool
+
+    // Contract owner can lock contract which prevents users from depositing or withdrawing
+    // 
 
 contract Vault {
 
 // ========================================= Variables and Instances 🧾
 
     address owner;
+    
+    //Chainlink pricefeed dolllrs per wei
+    AggregatorV3Interface internal priceFeed = AggregatorV3Interface(0xAB594600376Ec9fD91F8e885dADF0CE036862dE0);
 
     //vault variables
     uint public totalLPTokensMinted;
+    bool public isLocked;
 
     //Stablecoin Instances
     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
@@ -39,7 +48,7 @@ contract Vault {
     address constant am3CRV = 0xE7a24EF0C5e95Ffb0f6684b813A78F2a3AD7D171;
 
     //Uniswapv2 Router Instance
-    IUniswapV2Router02 public uniswapRouter = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+    IUniswapV2Router02 public uniswapRouter = IUniswapV2Router02(0x93bcDc45f7e62f89a8e901DC4A0E2c6C427D9F25);
     //AAVE Lending Pool instance
     IAaveLendingPool public aaveLendingPool = IAaveLendingPool(0x580D4Fdc4BF8f9b5ae2fb9225D584fED4AD5375c);
     //Curve Polygon AAVE Stablecoin Pool instance
@@ -69,9 +78,14 @@ contract Vault {
         balanceOf[_from] -= _shares;
     }
 
+    function lock(bool _lock) public {
+        require(msg.sender == owner);
+        isLocked =  _lock;
+    }
+
     //add ETH to vault
     function deposit() payable external {
-
+        require(!isLocked);
         uint shares;
         if (totalSupply == 0) {
             shares = msg.value;
@@ -83,12 +97,13 @@ contract Vault {
     }
 
     function withdraw(uint _shares) external {
+        require(!isLocked,"Contract Locked!");
         require(_shares <= balanceOf[msg.sender],"You dont have that many shares");
         uint amount = (_shares * address(this).balance) / totalSupply;
         _burn(msg.sender, _shares);
 
         //  ** send user something of amount **
-        
+
     }
 
 // ========================================= Strategy Execution Methods ⚔️
@@ -105,11 +120,36 @@ contract Vault {
         //get amount of ETH to spend per stablecoin (1/3)
         uint thirdOfETH = address(this).balance / 3;
 
-        // swap all ETH to USDC, USDT, and DAI from a DEX (Uniswap v2)
-        convertEthToStable(thirdOfETH,getPathForETHtoDAI());
-        convertEthToStable(thirdOfETH,getPathForETHtoUSDC());
-        convertEthToStable(thirdOfETH,getPathForETHtoUSDT());
+        
+        uint deadline = block.timestamp + 15; // using 'now' for convenience, for mainnet pass deadline from frontend!
 
+       // tokenAmount is the minimum amount of output tokens that must be received for the transaction not to revert.
+        //can use oracle
+        // calculate: pricefeed returns dollars per wei
+        // pricefeed * msg.value
+        (,int price,,,) = priceFeed.latestRoundData();
+
+        //gets price of matic
+        uint swapAmount = uint(price) * thirdOfETH;
+
+        uniswapRouter.swapExactETHForTokens{ value: thirdOfETH }(swapAmount, getPathForETHtoDAI(), address(this), deadline);
+        uniswapRouter.swapExactETHForTokens{ value: thirdOfETH }(swapAmount, getPathForETHtoUSDC(), address(this), deadline);
+        uniswapRouter.swapExactETHForTokens{ value: thirdOfETH }(swapAmount, getPathForETHtoUSDT(), address(this), deadline);
+
+        // refund leftover ETH to user
+        (bool success,) = msg.sender.call{ value: address(this).balance }("");
+        require(success, "refund failed");
+
+
+        // swap all ETH to USDC, USDT, and DAI from a DEX (Uniswap v2)
+ //       convertEthToStable(thirdOfETH,getPathForETHtoDAI());
+ //       convertEthToStable(thirdOfETH,getPathForETHtoUSDC());
+ //       convertEthToStable(thirdOfETH,getPathForETHtoUSDT());
+
+
+    //    uniswapRouter.swapExactETHForTokens{ value: msg.value }(tokenAmount, getPathForETHtoDAI(), address(this), deadline);
+    //    uniswapRouter.swapExactETHForTokens{ value: msg.value }(tokenAmount, getPathForETHtoUSDC(), address(this), deadline);
+    //    uniswapRouter.swapExactETHForTokens{ value: msg.value }(tokenAmount, getPathForETHtoUSDT(), address(this), deadline);
     }
 
      
@@ -134,27 +174,58 @@ contract Vault {
         uint aUsdt_balance = IERC20(maUSDT).balanceOf(address(this));
         uint[3] memory aaveTokenAmount = [aDai_balance,aUsdc_balance,aUsdt_balance];
 
+
         //calculate minumum amount of LP tokens to mint (required by add liquidity function)
         uint curve_expected_LP_token_amount = ICurve_AAVE_Stable_Pool(curvePool).calc_token_amount(aaveTokenAmount,true);
+
+        //approve
+        IERC20(maDAI).approve(address(curvePool), type(uint256).max);
+        IERC20(maUSDC).approve(address(curvePool), type(uint256).max);
+        IERC20(maUSDT).approve(address(curvePool), type(uint256).max);
 
         // Deposit funds into Curve's Polygon AAVE Stablecoin Pool
         uint actual_LP_token_amount = ICurve_AAVE_Stable_Pool(curvePool).add_liquidity(aaveTokenAmount,curve_expected_LP_token_amount);
         //update public LP token amount minted
         totalLPTokensMinted = actual_LP_token_amount;
+
+
+    }
+
+    function D_WithdrawFromCurve() public {
+      //  require(msg.sender == owner, "must be owner");
+      //  uint LPTokens = IERC20(am3CRV).balanceOf(address(this));
+
+       // uint curve_expected_LP_token_amount = ICurve_AAVE_Stable_Pool(curvePool).calc_token_amount(, false);
+       // ICurve_AAVE_Stable_Pool(curvePool).remove_liquidity(uint256 _amount, )
     }
 
 // ========================================= internal utility methods ✨
 
     //convert ETH to stable at a dex (uniswap🦄 V2 in this case, lots of tutorials readily available )
-    function convertEthToStable(uint tokenAmount, address[] memory stablecoin) internal {
+/*    function convertEthToStable(uint ethAmount, address[] memory stablecoin) internal {
+
 
         uint deadline = block.timestamp + 15; // using 'now' for convenience, for mainnet pass deadline from frontend!
-        uniswapRouter.swapETHForExactTokens{ value: msg.value }(tokenAmount, stablecoin, address(this), deadline);
+
+       // tokenAmount is the minimum amount of output tokens that must be received for the transaction not to revert.
+        //can use oracle
+        // calculate: pricefeed returns dollars per wei
+        // pricefeed * msg.value
+        (,int price,,,) = priceFeed.latestRoundData();
+
+        uint swapAmount = uint(price) * ethAmount;
+
+        // tokenAmount is the minimum amount of output tokens that must be received for the transaction not to revert.
+        //can use oracle
+        // calculate: pricefeed returns dollars per wei
+        // pricefeed * eth amount = amount of stablecoin to receive, since they are $1 each, might need to fix this later lol.
+        uniswapRouter.swapExactETHForTokens{ value: msg.value }(swapAmount, stablecoin, address(this), deadline);
     
         // refund leftover ETH to user
         (bool success,) = msg.sender.call{ value: address(this).balance }("");
         require(success, "refund failed");
     }
+*/
 
     function getPathForETHtoDAI() internal view returns (address[] memory) {
     address[] memory path = new address[](2);
